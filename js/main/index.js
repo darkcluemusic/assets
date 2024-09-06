faf2c20e76db0683fe97032af54ee7d907ba03b9
Dark Clue Music <darkcluemusic@gmail.com>
2024-09-06 23:28:58 +0700

    Create index.js

---
+++ js/main/index.js
@@ -0,0 +1,2 @@
+/* Compile by suryodwijayanto */
+function myFunction(i){i.classList.toggle("change"),$("#xmenu").slideToggle()}function mpl(){window.open("https://darkcluemusic.github.io/lyrics-maker/")}function mml(){window.open("https://darkcluemusic.github.io/music-lyrics-demo/")}
