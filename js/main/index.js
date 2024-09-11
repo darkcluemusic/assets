5727d3ee204e9bd4e44f750117a05c8398fcafe2
Dark Clue Music <darkcluemusic@gmail.com>
2024-09-12 01:30:05 +0700

    Update index.js

--- js/main/index.js
+++ js/main/index.js
@@ -1,2 +1,2 @@
 /* Compile by suryodwijayanto */
-function myFunction(menuburger){menuburger.classList.toggle("change");$("#xmenu").slideToggle()}function mpl(){window.open("https://darkcluemusic.github.io/lyrics-maker/")}function mml(){window.open("https://darkcluemusic.github.io/music-lyrics-demo/")}function jtl(){window.open("https://darkcluemusic.github.io/json-to-lrc/")}
+function myFunction(i){i.classList.toggle("change"),$("#xmenu").slideToggle()}function mpl(){window.location.href="https://darkcluemusic.github.io/lyrics-maker/"}function mml(){window.location.href="https://darkcluemusic.github.io/music-lyrics-demo/"}function jtl(){window.location.href="https://darkcluemusic.github.io/json-to-lrc/"}function dcmpl(){window.location.href="https://darkcluemusic.github.io/dcmplayer/"}
